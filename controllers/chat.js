b386f9be778117635ecd18ca4834a1687eb256fc
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2023-01-31 12:13:00 +0100

    public chat

---
+++ controllers/chat.js
@@ -0,0 +1,49 @@
+const express = require('express')
+
+const users = [];
+
+const addUser = (username) => {
+    const name = username.trim().toLowerCase()
+    const existingUser = users.find((u) => u === name);
+    if (!username.trim()) return { error: "Name is required" }
+    if (existingUser) {
+        return {
+            error: "Name is taken",
+        };
+    } else {
+        users.push(name)
+        return username
+    }
+};
+
+
+const chat = (io) => {
+    // console.log("livechat----->", io.opts)
+
+    io.on('connection', (socket) => {
+        // console.log("socket id,", socket.id)
+        socket.on('username', (username, next) => {
+            // console.log('username', username)
+            // io.emit('user joined', `${username} joined`)
+            let result = addUser(username)
+            if (result.error) {
+                return next(result.error)
+            } else {
+                io.emit("user", users)
+                socket.broadcast.emit('user joined', `${username} joined`)
+
+            }
+
+        })
+
+        socket.on("message", (message) => {
+            io.emit("message", message);
+        })
+
+        socket.on('disconect', () => {
+            console.log("user disconnected")
+        })
+    })
+}
+
+export default chat

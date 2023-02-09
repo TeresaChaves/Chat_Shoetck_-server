00d6b1d7b12eca9902ec9a8da35747da6b7bfb3e
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2023-02-09 17:01:00 +0100

    prepare doploy

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import chat from "./controllers/chat"
+import cors from "cors";
+
+require('dotenv').config()
+
+
+
+const app = express();
+const http = require("http").createServer(app);
+
+// socket io
+const io = require("socket.io")(http, {
+    path: "/socket.io",
+    cors: {
+        origin: [process.env.DOMAIN],
+        methods: ["GET", "POST"],
+        allowedHeaders: ["content-type"],
+    },
+})
+
+app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/api", (req, res) => {
+    res.send("THIS IS REST API!");
+});
+
+chat(io);
+
+const port = process.env.PORT || 8000;
+http.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+

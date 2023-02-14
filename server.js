515f8ac217c0aaecb46c45ad63e37fda94d49906
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2023-02-14 15:05:21 +0100

    change server env5

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// require = require("esm")(module);
-// module.exports = require("./index.js");
-const { createServer } = require('http');
-const express = require('express');
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ server: app.listen(port) });
+require = require("esm")(module);
+module.exports = require("./index.js");
+// const { createServer } = require('http');
+// const express = require('express');
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({ server: app.listen(port) });
